d51ca92098b90e46576f6e07aeda6ed1a9ae23ab
azmigproject <azmigproject@outlook.com>
2017-06-22 10:50:31 +0530

    updated script

--- Setup.sh
+++ Setup.sh
@@ -15,5 +15,6 @@ chmod 700 /root/scripts/keyVault.sh
 	echo "@reboot /root/scripts/keyVault.sh >> /root/scripts/log.txt" >> KeyVaultcron
 	crontab KeyVaultcron
 	rm KeyVaultcron
-	
-reboot
+
+#Execute script	
+/root/scripts/keyVault.sh
