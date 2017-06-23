70340cd11ee5a2f46831af85ac8d4cdb9a2519e9
azmigproject <azmigproject@outlook.com>
2017-06-23 12:32:01 +0530

    update

--- AzureCli.sh
+++ AzureCli.sh
@@ -1,10 +1,7 @@
 #sudo yum check-update; sudo yum install -y gcc libffi-devel python-devel openssl-devel
 #curl -s -L https://aka.ms/InstallAzureCli | bash
-wget https://repo.continuum.io/archive/Anaconda3-4.3.1-Linux-x86_64.sh
-chmod +x Anaconda3-4.3.1-Linux-x86_64.sh
-./Anaconda3-4.3.1-Linux-x86_64.sh
-curl "https://bootstrap.pypa.io/get-pip.py" -o "get-pip.py"
-python get-pip.py
-python -m pip install -U pip
-pip --version
+yum -y install epel-release
+yum -y install python34 python34-devel
+curl -O https://bootstrap.pypa.io/get-pip.py
+python3 get-pip.py
 pip install --user azure-cli
