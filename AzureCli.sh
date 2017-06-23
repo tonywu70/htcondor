d18ee6b4384f5f4f506100b7be3cccb6073e9122
azmigproject <azmigproject@outlook.com>
2017-06-23 12:12:12 +0530

    updated

--- AzureCli.sh
+++ AzureCli.sh
@@ -1,2 +1,2 @@
 sudo yum check-update; sudo yum install -y gcc libffi-devel python-devel openssl-devel
-curl -L https://aka.ms/InstallAzureCli | bash
+curl -s -L https://aka.ms/InstallAzureCli | bash
