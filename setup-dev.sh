a132f7188231cc1375fe88b3074a0849cc6ff0f1
azmigproject <azmigproject@outlook.com>
2017-06-28 15:31:26 +0530

    Update setup-dev.sh

--- setup-dev.sh
+++ setup-dev.sh
@@ -12,12 +12,12 @@ elif [[ $(echo "$release_info" | grep 'Debian') != "" ]]; then
 elif [[ $(echo "$release_info" | grep 'Scientfic Linux') != "" ]]; then 
     distro_type="centos";
 fi;
-
+mkdir -p /root/scripts
 case "$distro_type" in
     "centos" | "redhat")
-        echo $distro_type >> log.txt
-        sudo yum check-update >> log.txt
-        sudo yum install -y gcc libffi-devel python-devel openssl-devel >> log.txt
+        #Install azure cli 2.0
+        sudo yum check-update
+        sudo yum install -y gcc libffi-devel python-devel openssl-devel
         curl -L https://aka.ms/InstallAzureCli > InstallAzureCli.sh
         INSTALL_SCRIPT_URL="https://azurecliprod.blob.core.windows.net/install.py"
         install_script=$(mktemp -t azure_cli_install_tmp_XXXX) || exit
@@ -45,31 +45,25 @@ case "$distro_type" in
         then
         echo "Installation failed"
         fi
+        # Retrieve commands which were uploaded from custom data and create shell script
+        base64 --decode /var/lib/waagent/CustomData > "/root/scripts/keyVault.sh"
         ;;
     "debian" | "ubuntu")
-        #echo $distro_type
-        echo $distro_type >> log.txt
         #Install azure cli 2.0
         echo "deb [arch=amd64] https://packages.microsoft.com/repos/azure-cli/ wheezy main" | \
         sudo tee /etc/apt/sources.list.d/azure-cli.list
         sudo apt-key adv --keyserver packages.microsoft.com --recv-keys 417A0893
         sudo apt-get install apt-transport-https
         sudo apt-get update && sudo apt-get install azure-cli
+        # Retrieve commands which were uploaded from custom data and create shell script
+        cat /var/lib/cloud/instance/user-data.txt > "/root/scripts/keyVault.sh"
         ;;
 esac
-echo $PATH
-# Retrieve commands which were uploaded from custom data and create shell script
-mkdir -p /root/scripts
-cat /var/lib/cloud/instance/user-data.txt > "/root/scripts/keyVault.sh"
-
 # Register cron tab so when machine restart it downloads the secret from azure keyVault
 chmod 700 /root/scripts/keyVault.sh
 crontab -l > KeyVaultcron
-echo "@reboot /root/scripts/keyVault.sh >> /root/scripts/log.txt" >> KeyVaultcron
+echo "@reboot /root/scripts/keyVault.sh >> /root/scripts/keyVault.log" >> KeyVaultcron
 crontab KeyVaultcron
 rm KeyVaultcron
-
 #Execute script
-/root/scripts/keyVault.sh >> /root/scripts/log.txt
-
-exit 0
+/root/scripts/keyVault.sh >> /root/scripts/keyVault.log
