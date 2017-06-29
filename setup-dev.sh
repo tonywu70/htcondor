f433b56a7a2a1aa149b7387ae3dc79d08c712826
azmigproject <azmigproject@outlook.com>
2017-06-29 20:53:39 +0530

    Update setup-dev.sh

--- setup-dev.sh
+++ setup-dev.sh
@@ -1,11 +1,54 @@
 #!/bin/bash
 
+install_python_2_7()
+{
+    sudo yum -y update
+    yum groupinstall -y 'development tools'
+    yum install -y zlib-dev openssl-devel sqlite-devel bzip2-devel
+    yum install xz-libs
+    wget http://www.python.org/ftp/python/2.7.10/Python-2.7.10.tar.xz
+    xz -d Python-2.7.10.tar.xz
+    tar -xvf Python-2.7.10.tar
+    cd Python-2.7.10
+    ./configure --prefix=/usr/local 
+    make
+    make altinstall
+    ./configure --enable-shared --prefix=/usr/local LDFLAGS="-R /usr/local/lib"
+    ls -ltr /usr/bin/python*
+    ls -ltr /usr/local/bin/python*
+    export PATH="/usr/local/bin:$PATH"
+    echo $PATH
+}
+is_python_version_ok()
+{
+    python_version="$(python -c 'import platform; version=platform.python_version(); print(version)' | grep -Eo '2.[0-9]')"
+    echo "Existing version of Python: "
+    echo $(python -V)
+    echo "Using" $python_version "to compare..."
+    if ! command -v python >/dev/null 2>&1
+    then
+        echo "ERROR: Python not found. 'command -v python' returned failure."
+        return 1
+    elif [[ $python_version < '2.7' ]]
+    then
+        return 1
+    else
+        return 0
+    fi
+}
 install_azure_cli_centos_redhat()
 {
+    if ! is_python_version_ok
+    then
+        echo "Installing Python 2.7.10..."
+        install_python_2_7
+    fi
+    python -V
+    echo "Installing Azure CLI 2.0..."
     #Install azure cli 2.0
-    sudo yum check-update
+    sudo yum -y check-update
     sudo yum install -y gcc libffi-devel python-devel openssl-devel
-    curl -L https://aka.ms/InstallAzureCli > InstallAzureCli.sh
+    # curl -L https://aka.ms/InstallAzureCli > InstallAzureCli.sh
     INSTALL_SCRIPT_URL="https://azurecliprod.blob.core.windows.net/install.py"
     install_script=$(mktemp -t azure_cli_install_tmp_XXXX) || exit
     echo "Downloading Azure CLI install script from $INSTALL_SCRIPT_URL to $install_script."
@@ -13,15 +56,18 @@ install_azure_cli_centos_redhat()
     # check for the Python installation
     if ! command -v python >/dev/null 2>&1
     then
-    echo "ERROR: Python not found. 'command -v python' returned failure."
-    echo "If python is available on the system, add it to PATH. For example 'sudo ln -s /usr/bin/python3 /usr/bin/python'"
-    exit 1
+        echo "ERROR: Python not found. 'command -v python' returned failure."
+        echo "If python is available on the system, add it to PATH. For example 'sudo ln -s /usr/bin/python3 /usr/bin/python'"
+        exit 1
     fi
     # make python install script executable
     chmod 775 $install_script
-    echo "Running install script."
-    echo $install_script
-    yes "y" | $install_script
+    if ! command -v az >/dev/null 2>&1
+    then
+        echo "Running install script."
+        echo $install_script
+        yes "" | python2.7 $install_script
+    fi
 }
 set_path_centos_redhat()
 {
@@ -47,8 +93,8 @@ create_cron_job()
 {
     # Register cron tab so when machine restart it downloads the secret from azure keyVault
     chmod 755 /root/scripts/keyVault.sh
-    crontab -l > KeyVaultcron
-    echo "@reboot /root/scripts/keyVault.sh >> /root/scripts/keyVault.log" >> KeyVaultcron
+    crontab -l >> KeyVaultcron
+    echo '@reboot sudo env "PATH=$PATH" /root/scripts/keyVault.sh 1> /dev/null 2> /root/scripts/job.err' >> KeyVaultcron
     crontab KeyVaultcron
     rm KeyVaultcron
 }
@@ -57,7 +103,6 @@ download_secret()
     # Execute script
     /root/scripts/keyVault.sh >> /root/scripts/keyVault.log
 }
-
 release_info==$(cat /etc/*-release)
  
 if [[ $(echo "$release_info" | grep 'Red Hat') != "" ]]; then 
@@ -74,7 +119,7 @@ touch /root/scripts/keyVault.sh
 case "$distro_type" in
     "centos" | "redhat")
         install_azure_cli_centos_redhat
-        set_path_centos_redhat
+        #set_path_centos_redhat
         # Retrieve commands which were uploaded from custom data and create shell script
         base64 --decode /var/lib/waagent/CustomData > /root/scripts/keyVault.sh
         create_cron_job
