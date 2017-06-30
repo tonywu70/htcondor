525dc15bdbdac2d6fbc76a8e2228f4eca838eccf
azmigproject <azmigproject@outlook.com>
2017-06-30 19:26:50 +0530

    Update Setup.sh

--- Setup.sh
+++ Setup.sh
@@ -1,5 +1,18 @@
 #!/bin/bash
-
+distro_type=""
+get_distro_type()
+{
+    release_info==$(cat /etc/*-release) 
+    if [[ $(echo "$release_info" | grep 'Red Hat') != "" ]]; then 
+        distro_type="redhat"; 
+    elif [[ $(echo "$release_info" | grep 'CentOS') != "" ]] ; then 
+        distro_type="centos"; 
+    elif [[ $(echo "$release_info" | grep 'Ubuntu') != "" ]] ; then 
+        distro_type="ubuntu"; 
+    elif [[ $(echo "$release_info" | grep 'Scientfic Linux') != "" ]]; then 
+        distro_type="centos";
+    fi;
+}
 install_python_2_7()
 {
     sudo yum -y update
@@ -85,44 +98,38 @@ install_azure_cli_ubuntu()
 create_cron_job()
 {
     # Register cron tab so when machine restart it downloads the secret from azure downloadsecret
-    chmod 700 /root/scripts/downloadsecret.sh
     crontab -l > cronjobs
-    echo '@reboot /root/scripts/downloadsecret.sh >> /root/scripts/scriptexecution.log' >> cronjobs
+    echo '@reboot /root/scripts/downloadsecret.sh >> /root/scripts/execution.log' >> cronjobs
     crontab cronjobs
     rm cronjobs
 }
 download_secret()
 {
     # Execute script
-    /root/scripts/downloadsecret.sh >> /root/scripts/scriptexecution.log
+    /root/scripts/downloadsecret.sh >> /root/scripts/execution.log
 }
-release_info==$(cat /etc/*-release)
- 
-if [[ $(echo "$release_info" | grep 'Red Hat') != "" ]]; then 
-    distro_type="redhat"; 
-elif [[ $(echo "$release_info" | grep 'CentOS') != "" ]] ; then 
-    distro_type="centos"; 
-elif [[ $(echo "$release_info" | grep 'Ubuntu') != "" ]] ; then 
-    distro_type="ubuntu"; 
-elif [[ $(echo "$release_info" | grep 'Scientfic Linux') != "" ]]; then 
-    distro_type="centos";
-fi;
-mkdir -p /root/scripts
-touch /root/scripts/downloadsecret.sh
-case "$distro_type" in
-    "centos" | "redhat")
-        install_azure_cli_centos_redhat
-        # Retrieve commands which were uploaded from custom data and create shell script
-        base64 --decode /var/lib/waagent/CustomData > /root/scripts/downloadsecret.sh
-        create_cron_job
-        source ~/.bashrc
-        download_secret
-        ;;
-    "ubuntu")
-        install_azure_cli_ubuntu
-        # Retrieve commands which were uploaded from custom data and create shell script
-        cat /var/lib/cloud/instance/user-data.txt > "/root/scripts/downloadsecret.sh"
-        create_cron_job
-        download_secret
-        ;;
-esac
+main()
+{
+    mkdir -p /root/scripts
+    touch /root/scripts/downloadsecret.sh
+    chmod 700 /root/scripts/downloadsecret.sh
+    get_distro_type
+    case "$distro_type" in
+        "centos" | "redhat")
+            install_azure_cli_centos_redhat
+            # Retrieve commands which were uploaded from custom data and create shell script
+            base64 --decode /var/lib/waagent/CustomData > /root/scripts/downloadsecret.sh
+            create_cron_job
+            source ~/.bashrc
+            download_secret
+            ;;
+        "ubuntu")
+            install_azure_cli_ubuntu
+            # Retrieve commands which were uploaded from custom data and create shell script
+            cat /var/lib/cloud/instance/user-data.txt > "/root/scripts/downloadsecret.sh"
+            create_cron_job
+            download_secret
+            ;;
+    esac
+}
+main
