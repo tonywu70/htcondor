0181372fd011701a70037b2a53c194b3e73e1e83
azmigproject <azmigproject@outlook.com>
2017-07-14 14:58:23 +0530

    added script

--- AzureCli.sh
+++ AzureCli.sh
@@ -1,7 +1,2 @@
 sudo yum check-update; sudo yum install -y gcc libffi-devel python-devel openssl-devel
 curl -y -L https://aka.ms/InstallAzureCli | bash
-#yum -y install epel-release
-#yum -y install python34 python34-devel
-#curl -O https://bootstrap.pypa.io/get-pip.py
-#python get-pip.py
-#pip install --user azure-cli
